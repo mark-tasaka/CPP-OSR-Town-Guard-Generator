fcbb8826d23ff6cea36dd1cd1e1a5a829c02920b
Mark Tasaka <mark.tasaka@gmail.com>
2023-03-18 11:23:33 -0700

    Update 1.08

--- townGuardGenerator/TownGuard.cpp
+++ townGuardGenerator/TownGuard.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 #include <string>
+#include <array>
 #include "TownGuard.h"
+#include "DieRoll.h"
 using namespace std;
 
+TownGuard::TownGuard(int level, bool isMale)
+{
+	this->level = level;
+
+	unique_ptr<DieRoll> dieRolls = make_unique<DieRoll>();
+
+	array<int, 6> abilityScoreValues = dieRolls->generateAbilityScores();
+
+	unique_ptr<AbilityScore> abilityscores = make_unique<AbilityScore>(abilityScoreValues);
+
+	strength = abilityscores->getStrength();
+	constition = abilityscores->getConstitution();
+	dexterity = abilityscores->getDexterity();
+	intelligence = abilityscores->getIntelligence();
+	wisdom = abilityscores->getWisdom();
+	charisma = abilityscores->getCharisma();
+
+	strengthMod = abilityscores->getStrengthMod();
+	constitionMod = abilityscores->getConstitutionMod();
+	dexterityMod = abilityscores->getDexterityMod();
+	intelligenceMod = abilityscores->getIntelligenceMod();
+	wisdomMod = abilityscores->getWisdomMod();
+	charismaMod = abilityscores->getCharismaMod();
+
+	unique_ptr<FullName> guardname = make_unique<FullName>(isMale);
+	name = guardname->getFirstName() + " " + guardname->getSurname();
+	sex = guardname->getSex();
+}
--- townGuardGenerator/TownGuard.h
+++ townGuardGenerator/TownGuard.h
@@ -3,6 +3,38 @@
 
 #include <iostream>
 #include <string>
+#include "FullName.h"
+#include "AbilityScore.h"
+
+class TownGuard
+{
+public:
+	TownGuard(int level, bool isMale);
+	void setLevel(int level);
+	int getLevel() const;
+	void setTitle(int level);
+
+private:
+	int level;
+	int hitPoints;
+	string title;
+
+	int strength;
+	int constition;
+	int dexterity;
+	int intelligence;
+	int wisdom;
+	int charisma;
+	int strengthMod;
+	int constitionMod;
+	int dexterityMod;
+	int intelligenceMod;
+	int wisdomMod;
+	int charismaMod;
+
+	string name;
+	string sex;
+};
 
 using namespace std;
 
