872d169e4f42af7b131825dae10c48a336e23c67
Mark Tasaka <mark.tasaka@gmail.com>
2023-04-02 15:33:36 -0700

    Update 1.20

---
+++ townGuardGenerator/RunProgram.h
@@ -0,0 +1,24 @@
+#ifndef RUN_PROGRAM_H
+#define RUN_PROGRAM_H
+#include "TownGuard.h"
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+#include<memory>
+#include<array>
+
+class RunProgram
+{
+public:
+	void runProgram();
+	void generatePatrol();
+	void generatePatrols();
+	void generateSquad();
+	void generateSquads();
+	void generateWatch();
+	void generateCompany();
+};
+
+#endif // !RUN_PROGRAM_H
+
